fb76a8956a9c142f2df765bbe724407a5222d8db
xingfrontend <954605096@qq.com>
2020-02-26 20:14:25 +0800

    2.26

--- dist/app/apis/payNet/getNetInfo.js
+++ dist/app/apis/payNet/getNetInfo.js
@@ -5,7 +5,7 @@ const getNetInfo_1 = require("~/app/functions/payNet/getNetInfo");
 const validators_1 = require("~/app/validators/validators");
 const router = new Router();
 exports.router = router;
-router.get('/functions/getNetInfo', async (context) => {
+router.post('/functions/getNetInfo', async (context) => {
     await new validators_1.AccountValidator().validate(context);
     const infoOperations = getNetInfo_1.newGetNetInfo(context.request.body);
     context.body = await infoOperations.getInfo();
--- src/app/apis/payNet/getNetInfo.ts
+++ src/app/apis/payNet/getNetInfo.ts
@@ -2,7 +2,7 @@ import Router = require('koa-router');
 import { newGetNetInfo } from '~/app/functions/payNet/getNetInfo';
 import { AccountValidator } from '~/app/validators/validators'
 const router = new Router();
-router.get('/functions/getNetInfo', async context => {
+router.post('/functions/getNetInfo', async context => {
     await new AccountValidator().validate(context)
     const infoOperations = newGetNetInfo(context.request.body);
     context.body = await infoOperations.getInfo();
