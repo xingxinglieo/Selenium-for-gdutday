b46e38e8e29db962e64f2d613d24601e085ab891
xingfrontend <954605096@qq.com>
2020-02-26 15:16:55 +0800

    2020/02/26 周三 15:16:55.32

--- src/app/apis/payNet/getPayImg.ts
+++ src/app/apis/payNet/getPayImg.ts
@@ -4,6 +4,7 @@ import { newGetPayImg } from '~/app/functions/payNet/getPayImg';
 import { PayNetValidator } from '~/app/validators/validators'
 const router = new Router();
 router.get('/functions/getPayImg', async context => {
+    debugger;
     if (config.dev) console.log(context);
     await new PayNetValidator().validate(context)
     const srcOperations = newGetPayImg(context.request.body);
