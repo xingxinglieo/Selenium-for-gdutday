65a5e9fb05aef857b1447938c12622b573bbb33f
xingfrontend <954605096@qq.com>
2020-02-26 20:05:09 +0800

    2.26

--- dist/app/apis/payNet/getPayImg.js
+++ dist/app/apis/payNet/getPayImg.js
@@ -6,10 +6,10 @@ const getPayImg_1 = require("~/app/functions/payNet/getPayImg");
 const validators_1 = require("~/app/validators/validators");
 const router = new Router();
 exports.router = router;
-router.get('/functions/getPayImg', async (context) => {
+router.post('/functions/getPayImg', async (context) => {
     if (config_1.config.dev)
         console.log(context);
     await new validators_1.PayNetValidator().validate(context);
-    const srcOperations = getPayImg_1.newGetPayImg(context.request.query);
+    const srcOperations = getPayImg_1.newGetPayImg(context.request.body);
     context.body = await srcOperations.getPay();
 });
--- src/app/apis/payNet/getPayImg.ts
+++ src/app/apis/payNet/getPayImg.ts
@@ -3,10 +3,10 @@ import { config } from '~/config'
 import { newGetPayImg } from '~/app/functions/payNet/getPayImg';
 import { PayNetValidator } from '~/app/validators/validators'
 const router = new Router();
-router.get('/functions/getPayImg', async context => {
+router.post('/functions/getPayImg', async context => {
     if (config.dev) console.log(context);
     await new PayNetValidator().validate(context)
-    const srcOperations = newGetPayImg(context.request.query);
+    const srcOperations = newGetPayImg(context.request.body);
     context.body = await srcOperations.getPay();
 })
 export { router };
