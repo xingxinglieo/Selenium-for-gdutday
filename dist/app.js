5dbbaab863d84d7ce22179a2009e58daf44de64f
xingfrontend <954605096@qq.com>
2020-02-27 23:21:01 +0800

    2020/02/27 周四 23:21:01.52

--- dist/app.js
+++ dist/app.js
@@ -2,17 +2,19 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const AppLication = require("koa"); //应用对象
 const bodyParser = require("koa-bodyparser");
-const config_1 = require("~/config");
 const bestRequire = require("best-require"); //ts-ignore
 bestRequire(`${__dirname}`); //ts-ignore
+const config_1 = require("~/config");
 const consoleLog_1 = require("~/middlewares/consoleLog");
 const exception_1 = require("~/middlewares/exception");
+const confirmBrowser_1 = require("~/middlewares/confirmBrowser");
 const init_1 = require("~/core/init");
 const app = new AppLication();
 app.use(exception_1.catchError); //最后执行
 app.use(bodyParser());
 if (config_1.config.dev)
     app.use(consoleLog_1.consoleLog);
+app.use(confirmBrowser_1.confrimBroswer);
 init_1.default.initCore(app);
 const host = 3000;
 app.listen(host);
--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,19 @@
 import AppLication = require('koa'); //应用对象
 import bodyParser = require('koa-bodyparser');
-import { config } from '~/config'
 import bestRequire = require('best-require');//ts-ignore
 bestRequire(`${__dirname}`);//ts-ignore
 
+import { config } from '~/config'
 import { consoleLog } from '~/middlewares/consoleLog';
 import { catchError } from '~/middlewares/exception';
+import { confrimBroswer } from '~/middlewares/confirmBrowser'
 import InitManager from '~/core/init';
 
 const app = new AppLication();
 app.use(catchError);//最后执行
 app.use(bodyParser());
 if (config.dev) app.use(consoleLog);
+app.use(confrimBroswer);
 InitManager.initCore(app);
 
 const host = 3000;
