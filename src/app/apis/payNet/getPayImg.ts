4144711846cc20f6c65f9ebeff372fe78e1156b5
xingfrontend <954605096@qq.com>
2020-02-28 21:47:55 +0800

    2020/02/28 周五 21:47:55.02

--- src/app/apis/payNet/getPayImg.ts
+++ src/app/apis/payNet/getPayImg.ts
@@ -5,15 +5,15 @@ import { PayNetValidator } from "~/app/validators/validators";
 import { after23_30, before8 } from "~/lib/util";
 const router = new Router();
 router.post("/functions/getPayImg", async context => {
-    if (after23_30 && before8) {
-        context.body = { msg: "学校网站维护" };
-        context.status = 502;
-    } else {
-        if (config.dev) console.log(context);
-        await new PayNetValidator().validate(context);
-        const srcOperations = new GetPayImg(context.request.body);
-        context.body = await srcOperations.getPay();
-        srcOperations.browser.close().catch();
-    }
+    // if (after23_30 || before8) {
+    //     context.body = { msg: "学校网站维护" };
+    //     context.status = 502;
+    // } else {
+    if (config.dev) console.log(context);
+    await new PayNetValidator().validate(context);
+    const srcOperations = new GetPayImg(context.request.body);
+    context.body = await srcOperations.getPay();
+    srcOperations.browser.close().catch();
+    // }
 });
 export { router };
